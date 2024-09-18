0ed3d7342ac20680bb79cdb9a1ebbdd1c991ad3b
ThauannySuzuki <thauannysuzuki@gmail.com>
2024-09-18 11:15:28 -0300

    Troca de nome

--- src/main/java/br/com/thauanny/projetospring/controller/UsuarioController.java
+++ src/main/java/br/com/thauanny/projetospring/controller/UsuarioController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping (value = "/usuario")
+@RequestMapping (value = "/Usuario")
 public class UsuarioController {
 
     @Autowired
